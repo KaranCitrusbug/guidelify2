9b2691a264f8b744844473212871daba01029893
KaranCitrusbug <karan.citrusbug@gmail.com>
2024-03-20 18:22:52 +0530

    idex page

---
+++ assets/js/index.js
@@ -0,0 +1,96 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: false,
+        margin: 20,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1.5
+            },
+            1000:{
+                items:3
+            },
+            2000:{
+                items:3
+            }
+        }
+    });
+
+    $(".prevBtn").on("click", function(){
+        $(".owl-carousel").trigger("prev.owl.carousel");
+    });
+
+    $(".nextBtn").on("click", function(){
+        $(".owl-carousel").trigger("next.owl.carousel");
+    });
+});
+$(document).ready(function(){
+    $(".internSheep-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: false,
+        margin:40,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1.5
+            },
+            600:{
+                items:2.5
+            },
+            1000:{
+                items:4.6
+            },
+           
+        }
+    });
+
+    $(".previous").on("click", function(){
+        $(".internSheep-carousel").trigger("prev.owl.carousel");        
+    });
+
+    $(".next").on("click", function(){
+        $(".internSheep-carousel").trigger("next.owl.carousel");
+    });
+});
+
+$(document).ready(function(){
+    $(".popular-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: false,
+        margin:40,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1.5
+            },
+            600:{
+                items:2.5
+            },
+            1000:{
+                items:4.6
+            },
+           
+        }
+    });
+
+    $(".previous-button").on("click", function(){
+        $(".popular-carousel").trigger("prev.owl.carousel");        
+    });
+
+    $(".next-button").on("click", function(){
+        $(".popular-carousel").trigger("next.owl.carousel");
+    });
+});
