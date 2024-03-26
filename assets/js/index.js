91a2288120ce00fb8eeeecd8a7e0a676cc05a94d
KaranCitrusbug <karan.citrusbug@gmail.com>
2024-03-26 16:04:20 +0530

    responsive for mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,96 +1,105 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        loop: true,
-        nav: false,
-        dots: false,
-        margin: 20,
-        autoplay:true,
-        autoplayTimeout:5000,
-        autoplayHoverPause:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:1.5
-            },
-            1000:{
-                items:3
-            },
-            2000:{
-                items:3
-            }
-        }
-    });
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    margin: 20,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1.5,
+      },
+      1000: {
+        items: 3,
+      },
+      2000: {
+        items: 3,
+      },
+    },
+  });
 
-    $(".prevBtn").on("click", function(){
-        $(".owl-carousel").trigger("prev.owl.carousel");
-    });
+  $(".prevBtn").on("click", function () {
+    $(".owl-carousel").trigger("prev.owl.carousel");
+  });
 
-    $(".nextBtn").on("click", function(){
-        $(".owl-carousel").trigger("next.owl.carousel");
-    });
+  $(".nextBtn").on("click", function () {
+    $(".owl-carousel").trigger("next.owl.carousel");
+  });
 });
-$(document).ready(function(){
-    $(".internSheep-carousel").owlCarousel({
-        loop: true,
-        nav: false,
-        dots: false,
-        margin:40,
-        autoplay:true,
-        autoplayTimeout:5000,
-        autoplayHoverPause:true,
-        responsive:{
-            0:{
-                items:1.5
-            },
-            600:{
-                items:2.5
-            },
-            1000:{
-                items:4.6
-            },
-           
-        }
-    });
+$(document).ready(function () {
+  $(".internSheep-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    margin: 40,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1.5,
+      },
+      600: {
+        items: 2.5,
+      },
+      1000: {
+        items: 3.5,
+      },
+      1500: {
+        items: 4.8,
+      },
+    },
+  });
 
-    $(".previous").on("click", function(){
-        $(".internSheep-carousel").trigger("prev.owl.carousel");        
-    });
+  $(".previous").on("click", function () {
+    $(".internSheep-carousel").trigger("prev.owl.carousel");
+  });
 
-    $(".next").on("click", function(){
-        $(".internSheep-carousel").trigger("next.owl.carousel");
-    });
+  $(".next").on("click", function () {
+    $(".internSheep-carousel").trigger("next.owl.carousel");
+  });
 });
 
-$(document).ready(function(){
-    $(".popular-carousel").owlCarousel({
-        loop: true,
-        nav: false,
-        dots: false,
-        margin:40,
-        autoplay:true,
-        autoplayTimeout:5000,
-        autoplayHoverPause:true,
-        responsive:{
-            0:{
-                items:1.5
-            },
-            600:{
-                items:2.5
-            },
-            1000:{
-                items:4.6
-            },
-           
-        }
-    });
+$(document).ready(function () {
+  $(".popular-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    margin: 40,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1.5,
+      },
+      600: {
+        items: 2.5,
+      },
+      1000: {
+        items: 3.5,
+      },
+      1500: {
+        items: 4.8,
+      },
+    },
+  });
 
-    $(".previous-button").on("click", function(){
-        $(".popular-carousel").trigger("prev.owl.carousel");        
-    });
+  $(".previous-button").on("click", function () {
+    $(".popular-carousel").trigger("prev.owl.carousel");
+  });
 
-    $(".next-button").on("click", function(){
-        $(".popular-carousel").trigger("next.owl.carousel");
-    });
+  $(".next-button").on("click", function () {
+    $(".popular-carousel").trigger("next.owl.carousel");
+  });
 });
+
+function toggleAnswer(element) {
+  const answer = element.nextElementSibling;
+  answer.style.display = answer.style.display === "none" ? "block" : "none";
+}
